6232864233b8c2afbfe38a64a2cbfd00616034e2
naharweb15 <naharweb15@gmail.com>
2023-12-23 17:57:32 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,10 +26,11 @@
         {
           breakpoint: 1024,
           settings: {
+            
             slidesToShow: 3,
             slidesToScroll: 3,
             infinite: true,
-            dots: true
+            
           }
         },
         {
